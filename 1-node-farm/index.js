96aed7fac3905003569f1d8da8a99b54b1a7ea1e
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-10-08 22:23:28 +0530

    Synchronous Server module

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -34,6 +34,10 @@ const url = require('url');
 ///////////////////////////////
 // SERVER
 
+
+ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+ const dataObj = JSON.parse(data);
+      
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -42,7 +46,14 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW!');
     } else if(pathName === '/product') {
         res.end('This is the PRODUCT!');
-    } else {
+    } else if ( pathName === '/api') {
+
+    res.writeHead(200, {'content-type': 'application/json'});
+     res.end(data);
+
+      
+    }
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
